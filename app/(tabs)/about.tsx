a8ab02b50b6b8299943d3818ff887e612cae5396
Jnzinho <joaovitorjn@unochapeco.edu.br>
2025-03-24 23:33:04 -0300

    feat: chapter 2

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,17 +1,17 @@
 import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>About Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#25292e',
     flex: 1,
+    backgroundColor: '#25292e',
     justifyContent: 'center',
     alignItems: 'center',
   },
