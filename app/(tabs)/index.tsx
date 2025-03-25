42b3116ba6a723209b569a5da9348fb34e0eec86
Jnzinho <joaovitorjn@unochapeco.edu.br>
2025-03-24 23:43:02 -0300

    feat: halfed section 2 (i need to sleep)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,26 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { Link } from 'expo-router';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#25292e',
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#25292e',
     alignItems: 'center',
   },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
 });
