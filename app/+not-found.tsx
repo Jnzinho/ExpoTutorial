99391e21bbe808364e8e9116d01eb2c938108497
Jnzinho <joaovitorjn@unochapeco.edu.br>
2025-03-28 13:05:57 -0300

    feat: chapter 3

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -3,14 +3,14 @@ import { Link, Stack } from 'expo-router';
 
 export default function NotFoundScreen() {
   return (
-    <>
+    <View>
       <Stack.Screen options={{ title: 'Oops! Not Found' }} />
       <View style={styles.container}>
         <Link href="/" style={styles.button}>
           Go back to Home screen!
         </Link>
       </View>
-    </>
+    </View>
   );
 }
 
